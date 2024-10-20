c99693f17b0b5b07760875dc82ab73c5f450124f
Matt Jones <matthew3jones@gmail.com>
2024-10-19 18:25:52 -0700

    2.6 Creating Dashboards with Python

--- 3. Scripts/.ipynb_checkpoints/Citi_Bike_dashboard_Part1-checkpoint.py
+++ 3. Scripts/.ipynb_checkpoints/Citi_Bike_dashboard_Part1-checkpoint.py
@@ -18,10 +18,11 @@ from datetime import datetime as dt
 ############################## INITIAL SETTINGS #########################################
 #########################################################################################
 
-st.set_page_config(page_title = 'Citi Bikes Strategy Dashboard', layout='wide')
-st.title("Citi Bikes Strategy Dashboard")
-st.markdown("The dashboard will help with the expansion problems Citi Bikes currently faces")
-st.markdown("Right now, Citi Bikes runs into a situation where customers complain about bikes not being avaibale at certain times. This analysis aims to look at the potential reasons behind this.")
+st.set_page_config(page_title = 'Citi Bike Strategy Dashboard', layout='wide')
+st.title("Citi Bike Strategy Dashboard")
+st.markdown("#### This dashboard will help analyze current user behavior and identify expansion opportunities")
+st.markdown("_Citi Bike's popularity has increased since its launch in 2013, and during the Covid-19 pandemic, even more New York residents have seen the merit in bike sharing. The higher demand has led to distribution problems (i.e. too few bikes at popular stations, or too many bikes parked at stations making it impossible to return a bike)._")
+st.markdown("_We need to diagnose where these distribution issues are most likely to arise, and what the root of the problem may be._")
 
 
 
@@ -30,7 +31,6 @@ st.markdown("Right now, Citi Bikes runs into a situation where customers complai
 
 folderpath = r'/Users/matthewjones/Documents/CareerFoundry/Data Visualization with Python/Achievement 2/NY-CitiBike/2. Data/Processed Data'
 
-# df = pd.read_csv(os.path.join(folderpath, 'DB_reduced_bike_weather_data.csv'), index_col = 0, sep=', ')
 top20 = pd.read_csv(os.path.join(folderpath, 'DB_top20_stations.csv'), index_col = 0)
 df_temp = pd.read_csv(os.path.join(folderpath, 'DB_dualaxis_rides_temp.csv'), index_col = 0)
 
@@ -41,14 +41,18 @@ df_temp = pd.read_csv(os.path.join(folderpath, 'DB_dualaxis_rides_temp.csv'), in
 
 
 ####################### BAR CHART #######################
+st.header("Top 20 Most Popular Citi Bike Stations in New York 2022")
 fig = go.Figure(go.Bar(x = top20['start_station_name'],
                        y = top20['value'],
                        marker = {'color' : top20['value'], 'colorscale' : 'blues'}))
 
 fig.update_layout(
-    title = '<b>Top 20 Most Popular Citi Bike Stations in New York 2022</b>',
-    xaxis_title = '<b>Start Stations</b>',
-    yaxis_title ='<b>Total Trips</b>',
+    xaxis_title = dict(text = '<b>Start Stations</b>', 
+                        font = dict(size = 22)),
+    yaxis_title = dict(text = '<b>Total Trips</b>', 
+                        font = dict(size = 22)),
+    xaxis = dict(tickfont = dict(size=16)),
+    yaxis = dict(tickfont = dict(size=16)),
     width = 900, height = 600
 )
 fig.update_xaxes(
@@ -58,6 +62,7 @@ st.plotly_chart(fig, use_container_width = True)
 
 
 ####################### LINE CHART #######################
+st.header("Daily Bike Trips and Avergage NYC Temperature in 2022")
 line_fig = make_subplots(specs = [[{"secondary_y": True}]])
 
 
@@ -79,18 +84,19 @@ secondary_y = True
 )
 
 line_fig.update_layout(
-    title = dict(text = '<b>Daily Bike Trips and Avergage NYC Temperature in 2022</b>',
-                 font = dict(size = 18)),
     xaxis_title = '',
     yaxis1_title = dict(text = '<b>Bike Rides Daily</b>', 
-                        font = dict(size = 14, color = '#2B4B8D')),
+                        font = dict(size = 22, color = '#2B4B8D')),
     yaxis2_title = dict(text = '<b>Average Temperature (in C)</b>', 
-                        font = dict(size = 14, color = '#EB392A')),
+                        font = dict(size = 22, color = '#EB392A')),
     xaxis = dict(showgrid = False,
-                 range = [dt(2022, 1, 1), dt(2023, 1, 1)]),
-    yaxis1 = dict(showgrid = False),
+                 range = [dt(2022, 1, 1), dt(2023, 1, 1)],
+                 tickfont = dict(size=16)),
+    yaxis1 = dict(showgrid = False,
+                  tickfont = dict(size=14)),
     yaxis2 = dict(showgrid = False,
-                  zeroline = False),
+                  zeroline = False,
+                  tickfont = dict(size=14)),
     showlegend = False,
     margin = dict(pad = 10),
     width = 900, height = 500
@@ -103,12 +109,12 @@ st.plotly_chart(line_fig, use_container_width=True)
 
 
 ####################### GEOSPATIAL VISUALIZATION #######################
-# path_to_html = "Divvy Bike Trips Aggregated.html" 
+path_to_html = r'/Users/matthewjones/Documents/CareerFoundry/Data Visualization with Python/Achievement 2/NY-CitiBike/4. Visualizations/Citi_Bike_Trips_Aggregated.html'
 
-# # Read file and keep in variable
-# with open(path_to_html,'r') as f: 
-#     html_data = f.read()
+# Read file and keep in variable
+with open(path_to_html,'r') as f: 
+    html_data = f.read()
 
-# ## Show in webpage
-# st.header("Aggregated Bike Trips in Chicago")
-# st.components.v1.html(html_data,height=1000)
+## Show in webpage
+st.header("Aggregated Bike Trips in New York")
+st.components.v1.html(html_data,height=1000)
