ee10ff701002965f29bb6fa8a9d21c826300cbc9
Matt Jones <matthew3jones@gmail.com>
2024-10-24 15:04:26 -0700

    file paths

--- 3.Scripts/.ipynb_checkpoints/Citi_Bike_dashboard_Part2-checkpoint.py
+++ 3.Scripts/.ipynb_checkpoints/Citi_Bike_dashboard_Part2-checkpoint.py
@@ -44,11 +44,11 @@ accent = '#EB392A'
 ################################## IMPORT DATA ##########################################
 #########################################################################################
 
-folderpath = '/2.Data/Processed_Data'
+folderpath = r'/2.Data/Processed_Data'
 
-picturepath = '/4.Visualizations/Dashboard_Images'
+picturepath = r'/4.Visualizations/Dashboard_Images'
 
-htmlpath = '/4.Visualizations/Citi_Bike_Trips_Aggregated.html'
+htmlpath = r'/4.Visualizations/Citi_Bike_Trips_Aggregated.html'
 
 line_chart_data = pd.read_csv(os.path.join(folderpath, 'DB_line_chart_data.csv'), index_col = 0)
 bar_chart_start = pd.read_csv(os.path.join(folderpath, 'DB_bar_chart_start.csv'), index_col = 0)
